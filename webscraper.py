209b9af0dfb040d6dfb20c64e05983d28aa3ee9b
AlexanderCaza <ajmc03@outlook.com>
2024-12-09 13:46:17 -0800

    feat: Table headers now save into files, however iron alloys density table titles are bugged

--- webscraper.py
+++ webscraper.py
@@ -26,15 +26,12 @@ def get_urls_from_container(html, url):
     for child in children:
         href = child['href']
         root = os.getenv("ROOT")
-        print(href)
         if href.startswith('../..', 0, 3):
             href = href.replace('../..', root, 1)
             urls.append(href)
-            print(href)
         elif href.startswith('../', 0, 3):
             href = href.replace('..', root, 1)
             urls.append(href)
-            print(href)
         elif href.startswith('./', 0, 3):
             href = href.replace('.', root, 1)
             second_level_html = get_webpage_contents(href)
@@ -43,18 +40,52 @@ def get_urls_from_container(html, url):
                 urls.extend(second_level_hrefs)
             except:
                 urls.append(href)
-            print(href)
     return urls
+
+
+def save_table(url):
+    html = get_webpage_contents(url)
+    soup = BeautifulSoup(html, "html.parser")
+    containers = soup.find_all("table")
+    for container in containers:
+        table_name = container.find("caption").string.replace(' ', '_')
+        file = open("./tables/" + table_name + ".csv", 'a')
+        headers_intermediate = container.find_all("th")
+        for header_intermediate in headers_intermediate:
+            try:
+                header = header_intermediate.button.contents[0]
+                if (header == None):
+                    raise Exception("No Contents")
+            except:
+                try:
+                    header = header_intermediate.string
+                    if (header == None):
+                        raise Exception("No Contents")
+                except:
+                    string_candidates = header_intermediate.descendants
+                    string_assembled = ""
+                    for string_candidate in string_candidates:
+                        if type(string_candidate) == "NavigableString":
+                            string_assembled += string_candidate
+                    header = string_assembled
+            file.write(header.strip() + ",")
+
+
+def download_tables(urls):
+    for url in urls:
+        save_table(url)
+
 
 def main():
     # loading variables from .env file
     load_dotenv()
+    # Getting all webapges with tables to scrape
     root = os.getenv("ROOT")
     initial_url = root + os.getenv("STARTING_PATH")
     html = get_webpage_contents(initial_url)
-    webpages_to_scrape = get_urls_from_container(html, initial_url)
-    print(len(webpages_to_scrape))
-    print(webpages_to_scrape)
+    urls_to_scrape = get_urls_from_container(html, initial_url)
+    download_tables(urls_to_scrape)
+
 
 if __name__ == "__main__":
     main()
